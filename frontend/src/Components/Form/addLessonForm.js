d1c6d8fa6e1e95cc41a19dd02aef118d3b87cdb7
Cherniakov Iaroslav <yarcher@ukr.net>
2023-04-02 19:40:54 +0300

    remove old frontend

--- frontend/src/Components/Form/addLessonForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const addLessonForm = () => {
-  return (
-    <div>addLessonForm</div>
-  )
-}
-
-export default addLessonForm
--- frontend/src/Layout/Footer.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Footer = () => {
-  return <div className="text-center">&copy; All right reserved - 2022. </div>;
-};
